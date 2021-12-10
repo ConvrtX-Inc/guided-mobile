9101c51046cc41bba0d8a7cec59d843a0f85cd79
Vince Patrick <vince@convrtx.com>
2021-12-10 10:41:46 +0800

    GuidED Code Refactor
    - Removed unnecessary use of ScreenUnitUtil

--- lib/screens/calendar_managements/payments/find_booking_dates_with_calendar.dart
+++ lib/screens/calendar_managements/payments/find_booking_dates_with_calendar.dart
@@ -18,197 +18,194 @@ Future<dynamic> findBookingDatesWithCalendar(BuildContext context) {
       builder: (BuildContext context) {
         return StatefulBuilder(
             builder: (BuildContext context, StateSetter setState) {
-          return ScreenUtilInit(
-            builder: () => Container(
-              height: 726.h,
-              padding: EdgeInsets.symmetric(horizontal: 32.w, vertical: 42.h),
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(6),
-                              color: Colors.grey.withOpacity(0.2)),
-                          child: const Icon(
-                            Icons.arrow_back,
+          return Container(
+            height: 726.h,
+            padding: EdgeInsets.symmetric(horizontal: 32.w, vertical: 42.h),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6),
+                            color: Colors.grey.withOpacity(0.2)),
+                        child: const Icon(
+                          Icons.arrow_back,
+                          color: Colors.black,
+                        ),
+                      ),
+                      const Spacer(),
+                      InkWell(
+                        onTap: () {},
+                        child: Text(
+                          'Cancel',
+                          style: TextStyle(
+                              fontSize: 15.sp, fontWeight: FontWeight.w600),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 24.h,
+                  ),
+                  Text(
+                    'Find Booking Dates',
+                    style:
+                        TextStyle(fontSize: 24.sp, fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(
+                    height: 45.h,
+                  ),
+                  SizedBox(
+                    height: 56.h,
+                    width: 311.w,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        findBookingDatesWithCalendar(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            color: AppColors.primaryGreen,
+                          ),
+                          borderRadius: BorderRadius.circular(18),
+                        ),
+                        primary: Colors.white,
+                        onPrimary: Colors.white,
+                      ),
+                      child: Text(
+                        'November/2021',
+                        style: TextStyle(
+                          fontSize: 17.sp,
+                          fontWeight: FontWeight.w400,
+                          color: AppColors.primaryGreen,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 40.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      const Icon(Icons.arrow_back_ios),
+                      SizedBox(
+                        width: 250.w,
+                        height: 70.h,
+                        child: SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              children: <Widget>[
+                                getMonths(name: 'January', data: 0),
+                                SizedBox(
+                                  width: 5.w,
+                                ),
+                                getMonths(name: 'February', data: 0),
+                                SizedBox(
+                                  width: 5.w,
+                                ),
+                                getMonths(name: 'March', data: 0),
+                                SizedBox(
+                                  width: 5.w,
+                                ),
+                                getMonths(name: 'April', data: 2),
+                                SizedBox(
+                                  width: 5.w,
+                                ),
+                                getMonths(name: 'May', data: 1),
+                                SizedBox(
+                                  width: 5.w,
+                                ),
+                                getMonths(name: 'June', data: 0),
+                                SizedBox(
+                                  width: 5.w,
+                                ),
+                                getMonths(name: 'July', data: 0),
+                                SizedBox(
+                                  width: 5.w,
+                                ),
+                                getMonths(name: 'August', data: 3),
+                                SizedBox(
+                                  width: 5.w,
+                                ),
+                                getMonths(name: 'September', data: 0),
+                                SizedBox(
+                                  width: 5.w,
+                                ),
+                                getMonths(name: 'October', data: 0),
+                                SizedBox(
+                                  width: 5.w,
+                                ),
+                                getMonths(name: 'November', data: 0),
+                                SizedBox(
+                                  width: 5.w,
+                                ),
+                                getMonths(name: 'December', data: 0),
+                              ],
+                            )),
+                      ),
+                      const Icon(Icons.arrow_forward_ios),
+                    ],
+                  ),
+                  SizedBox(height: 40.h),
+                  Card(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                      child: TableCalendar<dynamic>(
+                        onDaySelected:
+                            (DateTime selectedDay, DateTime focusedDay) {
+                          setState(() {
+                            _focusedDay = focusedDay;
+                          });
+                        },
+                        // currentDay: _selectedDay,
+                        headerVisible: false,
+                        firstDay: DateTime.utc(2010, 10, 16),
+                        lastDay: DateTime.utc(2030, 3, 14),
+                        focusedDay: _focusedDay,
+                        calendarStyle: CalendarStyle(
+                          todayDecoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: AppColors.brightSun,
+                          ),
+                          todayTextStyle: const TextStyle(
                             color: Colors.black,
                           ),
                         ),
-                        const Spacer(),
-                        InkWell(
-                          onTap: () {},
-                          child: Text(
-                            'Cancel',
-                            style: TextStyle(
-                                fontSize: 15.sp, fontWeight: FontWeight.w600),
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 24.h,
-                    ),
-                    Text(
-                      'Find Booking Dates',
-                      style: TextStyle(
-                          fontSize: 24.sp, fontWeight: FontWeight.w600),
-                    ),
-                    SizedBox(
-                      height: 45.h,
-                    ),
-                    SizedBox(
-                      height: 56.h,
-                      width: 311.w,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          findBookingDatesWithCalendar(context);
-                        },
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            side: BorderSide(
-                              color: AppColors.primaryGreen,
-                            ),
-                            borderRadius: BorderRadius.circular(18),
-                          ),
-                          primary: Colors.white,
-                          onPrimary: Colors.white,
-                        ),
-                        child: Text(
-                          'November/2021',
-                          style: TextStyle(
-                            fontSize: 17.sp,
-                            fontWeight: FontWeight.w400,
-                            color: AppColors.primaryGreen,
-                          ),
-                        ),
                       ),
                     ),
-                    SizedBox(height: 40.h),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        const Icon(Icons.arrow_back_ios),
-                        SizedBox(
-                          width: 250.w,
-                          height: 70.h,
-                          child: SingleChildScrollView(
-                              scrollDirection: Axis.horizontal,
-                              child: Row(
-                                children: <Widget>[
-                                  getMonths(name: 'January', data: 0),
-                                  SizedBox(
-                                    width: 5.w,
-                                  ),
-                                  getMonths(name: 'February', data: 0),
-                                  SizedBox(
-                                    width: 5.w,
-                                  ),
-                                  getMonths(name: 'March', data: 0),
-                                  SizedBox(
-                                    width: 5.w,
-                                  ),
-                                  getMonths(name: 'April', data: 2),
-                                  SizedBox(
-                                    width: 5.w,
-                                  ),
-                                  getMonths(name: 'May', data: 1),
-                                  SizedBox(
-                                    width: 5.w,
-                                  ),
-                                  getMonths(name: 'June', data: 0),
-                                  SizedBox(
-                                    width: 5.w,
-                                  ),
-                                  getMonths(name: 'July', data: 0),
-                                  SizedBox(
-                                    width: 5.w,
-                                  ),
-                                  getMonths(name: 'August', data: 3),
-                                  SizedBox(
-                                    width: 5.w,
-                                  ),
-                                  getMonths(name: 'September', data: 0),
-                                  SizedBox(
-                                    width: 5.w,
-                                  ),
-                                  getMonths(name: 'October', data: 0),
-                                  SizedBox(
-                                    width: 5.w,
-                                  ),
-                                  getMonths(name: 'November', data: 0),
-                                  SizedBox(
-                                    width: 5.w,
-                                  ),
-                                  getMonths(name: 'December', data: 0),
-                                ],
-                              )),
-                        ),
-                        const Icon(Icons.arrow_forward_ios),
-                      ],
-                    ),
-                    SizedBox(height: 40.h),
-                    Card(
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                        child: TableCalendar<dynamic>(
-                          onDaySelected:
-                              (DateTime selectedDay, DateTime focusedDay) {
-                            setState(() {
-                              _focusedDay = focusedDay;
-                            });
-                          },
-                          // currentDay: _selectedDay,
-                          headerVisible: false,
-                          firstDay: DateTime.utc(2010, 10, 16),
-                          lastDay: DateTime.utc(2030, 3, 14),
-                          focusedDay: _focusedDay,
-                          calendarStyle: CalendarStyle(
-                            todayDecoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: AppColors.brightSun,
-                            ),
-                            todayTextStyle: const TextStyle(
-                              color: Colors.black,
-                            ),
+                  ),
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: 60.h,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        findBookingDatesWithTime(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            color: AppColors.silver,
                           ),
+                          borderRadius: BorderRadius.circular(18),
                         ),
+                        primary: AppColors.primaryGreen,
+                        onPrimary: Colors.white,
+                      ),
+                      child: Text(
+                        'View Schedule',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700, fontSize: 16.sp),
                       ),
                     ),
-                    SizedBox(
-                      height: 20.h,
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      height: 60.h,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          findBookingDatesWithTime(context);
-                        },
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            side: BorderSide(
-                              color: AppColors.silver,
-                            ),
-                            borderRadius: BorderRadius.circular(18),
-                          ),
-                          primary: AppColors.primaryGreen,
-                          onPrimary: Colors.white,
-                        ),
-                        child: Text(
-                          'View Schedule',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w700, fontSize: 16.sp),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-            designSize: const Size(375, 812),
           );
         });
       });
@@ -236,26 +233,26 @@ Widget getMonths({required String name, required int data}) {
             ),
           ),
           if (data >= 1)
-          Positioned(
-            right: -5,
-            top: -7,
-            child: Container(
-              height: 28.h,
-              width: 28.h,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.green,
+            Positioned(
+              right: -5,
+              top: -7,
+              child: Container(
+                height: 28.h,
+                width: 28.h,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.green,
+                ),
+                child: Center(
+                    child: Text(
+                  '$data',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 13.sp,
+                      fontWeight: FontWeight.w500),
+                )),
               ),
-              child: Center(
-                  child: Text(
-                '$data',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 13.sp,
-                    fontWeight: FontWeight.w500),
-              )),
-            ),
-          )
+            )
         ]),
       ),
     );
--- lib/screens/signin_signup/continue_with_phone.dart
+++ lib/screens/signin_signup/continue_with_phone.dart
@@ -26,138 +26,132 @@ class _ContinueWithPhoneState extends State<ContinueWithPhone> {
     final double height = MediaQuery.of(context).size.height;
     final double width = MediaQuery.of(context).size.width;
 
-    return ScreenUtilInit(
-        builder: () => Scaffold(
-          appBar: AppBar(
-            leading: IconButton(
-              icon: const Icon(
-                Icons.chevron_left,
-                color: Colors.black,
-              ),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            elevation: 0,
-            backgroundColor: Colors.white,
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.chevron_left,
+            color: Colors.black,
           ),
-          body: SafeArea(
-            child: SizedBox(
-              width: width,
-              height: height,
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(30.w, 10.h, 30.w, 10.h),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        AppTextConstants.continueWithPhone,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 25,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20.h,
-                      ),
-                      Text(
-                        AppTextConstants.codeDescription,
-                        style: const TextStyle(
-                          color: Colors.black,
-                          fontFamily: 'Gilroy',
-                          fontWeight: FontWeight.w400,
-                          fontSize: 16,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 40.h,
-                      ),
-                      Text(
-                        AppTextConstants.enterPhoneNumber,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 15
-                        ),
-                      ),
-                      SizedBox(
-                        height: 15.h
-                      ),
-                      TextField(
-                        controller: phoneController,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          hintText: AppTextConstants.phoneNumberHint,
-                          prefixIcon: SizedBox(
-                            child: CountryCodePicker(
-                              onChanged: _onCountryChange,
-                              initialSelection: AppTextConstants.defaultCountry,
-                              favorite: ['+1', 'US'],
-                            ),
-                          ),
-                          hintStyle: TextStyle(
-                            color: AppColors.grey,
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(14.r),
-                            borderSide: BorderSide(color: Colors.grey, width: 0.2.w),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-          bottomNavigationBar: Padding(
-            padding: const EdgeInsets.all(20),
-            child: SizedBox(
-              width: width,
-              height: 60.h,
-              child: ElevatedButton(
-                onPressed: _apiCall,
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                      color: AppColors.cloud,
-                    ),
-                    borderRadius: BorderRadius.circular(18), // <-- Radius
-                  ),
-                  primary: AppColors.primaryGreen,
-                  onPrimary: Colors.white, // <-- Splash color
-                ),
-                child: Text(
-                  AppTextConstants.continueText,
-                  style: const TextStyle(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      body: SafeArea(
+        child: SizedBox(
+          width: width,
+          height: height,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(30.w, 10.h, 30.w, 10.h),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    AppTextConstants.continueWithPhone,
+                    style: const TextStyle(
                       fontWeight: FontWeight.bold,
-                      fontSize: 16
+                      fontSize: 25,
+                    ),
                   ),
-                ),
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  Text(
+                    AppTextConstants.codeDescription,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Gilroy',
+                      fontWeight: FontWeight.w400,
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40.h,
+                  ),
+                  Text(
+                    AppTextConstants.enterPhoneNumber,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w600, fontSize: 15),
+                  ),
+                  SizedBox(height: 15.h),
+                  TextField(
+                    controller: phoneController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      hintText: AppTextConstants.phoneNumberHint,
+                      prefixIcon: SizedBox(
+                        child: CountryCodePicker(
+                          onChanged: _onCountryChange,
+                          initialSelection: AppTextConstants.defaultCountry,
+                          favorite: ['+1', 'US'],
+                        ),
+                      ),
+                      hintStyle: TextStyle(
+                        color: AppColors.grey,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14.r),
+                        borderSide:
+                            BorderSide(color: Colors.grey, width: 0.2.w),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
         ),
-        designSize: const Size(375, 812)
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(20),
+        child: SizedBox(
+          width: width,
+          height: 60.h,
+          child: ElevatedButton(
+            onPressed: _apiCall,
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                side: BorderSide(
+                  color: AppColors.cloud,
+                ),
+                borderRadius: BorderRadius.circular(18), // <-- Radius
+              ),
+              primary: AppColors.primaryGreen,
+              onPrimary: Colors.white, // <-- Splash color
+            ),
+            child: Text(
+              AppTextConstants.continueText,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+          ),
+        ),
+      ),
     );
   }
 
   /// Method for getting the country code
-  void _onCountryChange(CountryCode countryCode) => _dialCode = countryCode.dialCode.toString();
+  void _onCountryChange(CountryCode countryCode) =>
+      _dialCode = countryCode.dialCode.toString();
 
   /// Method for calling the API
   void _apiCall() {
     // ApiCalls.sendCode(context, _dialCode + phoneController.text); // Temporary commented out for setting up other API Integration
     Navigator.push(
         context,
-        MaterialPageRoute(builder: (BuildContext context) => SignupVerify(phoneNumber: _dialCode + phoneController.text))
-    );
+        MaterialPageRoute(
+            builder: (BuildContext context) =>
+                SignupVerify(phoneNumber: _dialCode + phoneController.text)));
   }
 
   @override
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
     properties.add(DiagnosticsProperty<bool>('isPhoneValid', isPhoneValid));
-    properties.add(DiagnosticsProperty<TextEditingController>('phoneController', phoneController));
+    properties.add(DiagnosticsProperty<TextEditingController>(
+        'phoneController', phoneController));
   }
 }
