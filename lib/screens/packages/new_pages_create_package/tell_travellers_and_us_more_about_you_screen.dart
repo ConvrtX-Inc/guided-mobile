8cfe7d794ea92b5a847d42da605f2641521fa5c4
Stephane Segning Lambou <stephane.segning@ssegning.com>
2022-09-22 14:03:24 +0200

    Page "Tell Travellers" done

--- lib/screens/packages/new_pages_create_package/tell_travellers_and_us_more_about_you_screen.dart
+++ lib/screens/packages/new_pages_create_package/tell_travellers_and_us_more_about_you_screen.dart
@@ -4,7 +4,9 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:guided/common/widgets/package_widgets.dart';
+import 'package:guided/common/widgets/text_flieds.dart';
 import 'package:guided/constants/app_text_style.dart';
 import 'package:guided/utils/package.util.dart';
 
@@ -49,6 +51,73 @@ class _TellTravellersAndUsMoreAboutYouScreenState
                   'Tell Travellers  & us more about you'),
               SizedBox(
                 height: 20.h,
+              ),
+              Text(
+                'How are you leading this Adventure',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              FormBuilderRadioGroup(
+                name: 'independant',
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(context),
+                ]),
+                options: [
+                  FormBuilderFieldOption(
+                    value: 'independant',
+                    child: ListTile(
+                      title: Text("I'm an independent guide"),
+                    ),
+                  ),
+                  FormBuilderFieldOption(
+                    value: 'not-independant',
+                    child: ListTile(
+                      title: Text("I have a team guides who helps me"),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              Text(
+                'Your personal profile',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              Text(
+                'Use your legal name and provide a photo that cleary shows your face (not a logo)',
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              ListTile(
+                title: Text('SSegning Lambou'),
+                subtitle: Text('Location'),
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              Text(
+                'What makes you qualified to lead this Adventure?',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              AppTextField(
+                name: 'youQualified',
+                maxLines: 6,
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(context),
+                ]),
+              ),
+              SizedBox(
+                height: 40.h,
               ),
             ],
           ),
