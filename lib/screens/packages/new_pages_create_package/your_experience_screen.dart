41849007670b9daa3dfd2c8a5d2fafbdd7420d92
Stephane Segning Lambou <stephane.segning@ssegning.com>
2022-09-22 12:17:54 +0200

    Fix scrolling

--- lib/screens/packages/new_pages_create_package/your_experience_screen.dart
+++ lib/screens/packages/new_pages_create_package/your_experience_screen.dart
@@ -35,6 +35,7 @@ class _YourExperienceScreenState extends State<YourExperienceScreen> {
   @override
   Widget build(BuildContext context) {
     return PackageWidgetLayout(
+      disableSpacer: true,
       buttonText: 'Next',
       onButton: () {
         if (_formKey.currentState?.validate() != true) {
@@ -43,117 +44,111 @@ class _YourExperienceScreenState extends State<YourExperienceScreen> {
 
         navigateTo(context, AppRoutes.THE_PERKS, _formKey.currentState!.value);
       },
-      child: SingleChildScrollView(
-        child: FormBuilder(
-          key: _formKey,
-          onChanged: () {
-            _formKey.currentState!.save();
-          },
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              HeaderText.headerTextLight('Your Experience'),
-              SingleChildScrollView(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 20.h,
-                    ),
-                    Stack(
-                      children: [
-                        PackageImageWidget(assetUrl: activity.featureImage),
-                        Positioned(
-                          child: CircleAvatar(
-                            backgroundImage: AssetImage(activity.path),
-                            backgroundColor: Colors.transparent,
-                            radius: 40.0,
-                          ),
-                          bottom: 0.0,
-                          left: 0.0,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 20.h,
-                        ),
-                        Text(
-                            "At GuidED, we look for people who are passionate about the outdoors, who want to share their unique stories, skills and perspectives, and who have the knowledge that only a local or expert would have. ",
-                            style: TextStyle(fontSize: 16)),
-                        SizedBox(
-                          height: 20.h,
-                        ),
-                        Text(
-                            "This could consist of:",
-                            style: TextStyle(fontSize: 16)),
-                        SizedBox(
-                          height: 20.h,
-                        ),
-                        Text(
-                            "⚫️ Being a knowledgeable, well-rounded outdoor enthusiast ",
-                            style: TextStyle(fontSize: 16)),
-                        SizedBox(
-                          height: 20.h,
-                        ),
-                        Text(
-                            "⚫️ Having specialized training & skills",
-                            style: TextStyle(fontSize: 16)),
-                        SizedBox(
-                          height: 20.h,
-                        ),
-                        Text(
-                            "⚫️ Having certificates or achievements in your area of expertise",
-                            style: TextStyle(fontSize: 16)),
-                        SizedBox(
-                          height: 20.h,
-                        ),
-                        Text(
-                            "Travellers love to learn something new on every Adventure",
-                            style: TextStyle(fontSize: 16)),
-                        SizedBox(
-                          height: 20.h,
-                        ),
-                        Divider(),
-                        SizedBox(
-                          height: 20.h,
-                        ),
-                        Text(
-                          'Have you lead this Adventure before?',
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                        ),
-                        SizedBox(
-                          height: 20.h,
-                        ),
-                        ListTile(
-                          leading: Checkbox(value: false, onChanged: (bool? value) {  },),
-                          title: Text(
-                            'Yes, I have lead this Adventure professionally',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ),
-                        ListTile(
-                          leading: Checkbox(value: false, onChanged: (bool? value) {  },),
-                          title: Text(
-                            'Yes, I have lead this Adventure before, but for only friends and family. ',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ),
-                        ListTile(
-                          leading: Checkbox(value: false, onChanged: (bool? value) {  },),
-                          title: Text(
-                            'No, I have never lead an Adventure before, but am excited to get started!',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        )
-                      ],
-                    )
-                  ],
+      child: FormBuilder(
+        key: _formKey,
+        onChanged: () {
+          _formKey.currentState!.save();
+        },
+        child: Expanded(
+    child: ListView(
+          children: <Widget>[
+            HeaderText.headerTextLight('Your Experience'),
+
+            SizedBox(
+              height: 20.h,
+            ),
+            Stack(
+              children: [
+                PackageImageWidget(assetUrl: activity.featureImage),
+                Positioned(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage(activity.path),
+                    backgroundColor: Colors.transparent,
+                    radius: 40.0,
+                  ),
+                  bottom: 0.0,
+                  left: 0.0,
                 ),
-              )
-            ],
-          ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 20.h,
+                ),
+                Text(
+                    "At GuidED, we look for people who are passionate about the outdoors, who want to share their unique stories, skills and perspectives, and who have the knowledge that only a local or expert would have. ",
+                    style: TextStyle(fontSize: 16)),
+                SizedBox(
+                  height: 20.h,
+                ),
+                Text(
+                    "This could consist of:",
+                    style: TextStyle(fontSize: 16)),
+                SizedBox(
+                  height: 20.h,
+                ),
+                Text(
+                    "⚫️ Being a knowledgeable, well-rounded outdoor enthusiast ",
+                    style: TextStyle(fontSize: 16)),
+                SizedBox(
+                  height: 20.h,
+                ),
+                Text(
+                    "⚫️ Having specialized training & skills",
+                    style: TextStyle(fontSize: 16)),
+                SizedBox(
+                  height: 20.h,
+                ),
+                Text(
+                    "⚫️ Having certificates or achievements in your area of expertise",
+                    style: TextStyle(fontSize: 16)),
+                SizedBox(
+                  height: 20.h,
+                ),
+                Text(
+                    "Travellers love to learn something new on every Adventure",
+                    style: TextStyle(fontSize: 16)),
+                SizedBox(
+                  height: 20.h,
+                ),
+                Divider(),
+                SizedBox(
+                  height: 20.h,
+                ),
+                Text(
+                  'Have you lead this Adventure before?',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                ListTile(
+                  leading: Checkbox(value: false, onChanged: (bool? value) {  },),
+                  title: Text(
+                    'Yes, I have lead this Adventure professionally',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                ListTile(
+                  leading: Checkbox(value: false, onChanged: (bool? value) {  },),
+                  title: Text(
+                    'Yes, I have lead this Adventure before, but for only friends and family. ',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                ListTile(
+                  leading: Checkbox(value: false, onChanged: (bool? value) {  },),
+                  title: Text(
+                    'No, I have never lead an Adventure before, but am excited to get started!',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
         ),
       ),
       page: 4,
