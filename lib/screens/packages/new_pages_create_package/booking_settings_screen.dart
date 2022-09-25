a1820d7d59aaa3d1ea7599551ee3bf65cf518560
Emmanuel Dadem <emmadadem@gmail.com>
2022-09-25 17:09:11 +0200

    Complete booking_settings_screen.dart & guided_cancellation_policy_screen.dart

--- lib/screens/packages/new_pages_create_package/booking_settings_screen.dart
+++ lib/screens/packages/new_pages_create_package/booking_settings_screen.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:guided/common/widgets/dividers.dart';
 import 'package:guided/common/widgets/package_widgets.dart';
 import 'package:guided/constants/app_routes.dart';
 import 'package:guided/constants/app_text_style.dart';
@@ -21,9 +21,12 @@ class BookingSettingsScreen extends StatefulWidget {
 class _BookingSettingsScreenState extends State<BookingSettingsScreen> {
   final _formKey = GlobalKey<FormBuilderState>();
 
+  List<String> numberOfGuestOptions = ['1', '2', '3', '4',' 5'];
+
   @override
   Widget build(BuildContext context) {
     return PackageWidgetLayout(
+      disableSpacer: true,
       buttonText: 'Next',
       onButton: () {
         if (_formKey.currentState?.validate() != true) {
@@ -34,18 +37,48 @@ class _BookingSettingsScreenState extends State<BookingSettingsScreen> {
             _formKey.currentState!.value);
       },
       page: 19,
-      child: SingleChildScrollView(
-        child: FormBuilder(
-          key: _formKey,
-          onChanged: () {
-            _formKey.currentState!.save();
-          },
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      child: FormBuilder(
+        key: _formKey,
+        onChanged: () {
+          _formKey.currentState!.save();
+        },
+        child: Expanded(
+          child: ListView(
             children: <Widget>[
               HeaderText.headerTextLight("Booking Settings"),
-              SizedBox(
-                height: 20.h,
+              AppSizedBox(h: 20),
+              Text(
+                "Think about how much time you need to prepare for your Adventure.  If you have lots to prepare, and need more time, we recommend setting a longer cut off time.  If you are ready to go and don't have much to prepare, then go shorter.",
+                style: TextStyle(fontSize: 16),
+              ),
+              AppSizedBox(h: 40),
+              Text(
+                "Cutoff time",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "If an Adventure is booked, this is how much time you need to prepare.",
+                style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+              ),
+              FormBuilderDropdown<String>(
+                name: 'cutoffTime',
+                decoration: InputDecoration(
+                  labelText: 'Choose a cutoff time',
+                  suffix: IconButton(
+                    icon: const Icon(Icons.close),
+                    onPressed: () {
+                      _formKey.currentState!.fields['cutoffTime']?.reset();
+                    },
+                  ),
+                  hintText: 'Choose a cutoff time',
+                ),
+                items: numberOfGuestOptions
+                    .map((numberOption) => DropdownMenuItem(
+                  alignment: AlignmentDirectional.center,
+                  value: numberOption,
+                  child: Text(numberOption + " hour(s) before start time"),
+                ))
+                    .toList(),
               ),
             ],
           ),
--- lib/screens/packages/new_pages_create_package/guided_cancellation_policy_screen.dart
+++ lib/screens/packages/new_pages_create_package/guided_cancellation_policy_screen.dart
@@ -19,13 +19,16 @@ class GuidedCancellationPolicyScreen extends StatefulWidget {
       _GuidedCancellationPolicyScreenState();
 }
 
-class _GuidedCancellationPolicyScreenState
-    extends State<GuidedCancellationPolicyScreen> {
+class _GuidedCancellationPolicyScreenState extends State<GuidedCancellationPolicyScreen> {
   final _formKey = GlobalKey<FormBuilderState>();
+
+  String firstParagraph = "Travellers can cancel until 7 days before the Adventure start time for a full refund, or within 24 hours of booking as long as the booking is made more than 7 days before the start time.  If cancelled within 7 days of the start time of the Adventure, there will be a 50% refund.  If cancelled within 48 hours of the start time of the Adventure, there will be no refund.";
+  String secondParagraph = "If a Guide cancels an Adventure, the following provisions govern the cancellation, unless there are Exceptional Circumstances: The Traveler will receive a full refund of the Adventure Fee for the Adventure. If the Guide makes a cancellation more than 48 hours before the start time of the Adventure, there will be no penalty. If the Guide makes a cancellation within 24 hours of the start time of the Adventure, the Guide will be charged a penalty of 20%, with the penalty to be deducted from future Payouts. The Guide’s calendar will be blacked out for the dates on which they have cancelled so that they will no longer be able to host an Adventure on that date; and An automatic review/rating will be posted to their page citing the cancellation. - If a Guide cancels within 24 hours of the start time of an Adventure more than 3 times they may be removed from the Services.";
 
   @override
   Widget build(BuildContext context) {
     return PackageWidgetLayout(
+      disableSpacer: true,
       buttonText: 'Continue',
       onButton: () {
         if (_formKey.currentState?.validate() != true) {
@@ -36,19 +39,29 @@ class _GuidedCancellationPolicyScreenState
             _formKey.currentState!.value);
       },
       page: 20,
-      child: SingleChildScrollView(
-        child: FormBuilder(
-          key: _formKey,
-          onChanged: () {
-            _formKey.currentState!.save();
-          },
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      child: FormBuilder(
+        key: _formKey,
+        onChanged: () {
+          _formKey.currentState!.save();
+        },
+        child: Expanded(
+          child: ListView(
             children: <Widget>[
               HeaderText.headerTextLight("GuidED's Cancellation Policy"),
               SizedBox(
                 height: 20.h,
               ),
+              Text(
+                firstParagraph,
+                  style: TextStyle(fontSize: 16)
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              Text(
+                  secondParagraph,
+                  style: TextStyle(fontSize: 16)
+              )
             ],
           ),
         ),
