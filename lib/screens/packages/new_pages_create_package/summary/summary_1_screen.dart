34ee82f5cb99534558403f749856ffbfe5ff894d
Emmanuel Dadem <emmadadem@gmail.com>
2022-09-25 18:04:31 +0200

    Complete summary_1_screen.dart

--- lib/screens/packages/new_pages_create_package/summary/summary_1_screen.dart
+++ lib/screens/packages/new_pages_create_package/summary/summary_1_screen.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:guided/common/widgets/dividers.dart';
 import 'package:guided/common/widgets/package_widgets.dart';
 import 'package:guided/constants/app_routes.dart';
 import 'package:guided/constants/app_text_style.dart';
@@ -20,22 +21,99 @@ class _Summary1ScreenState extends State<Summary1Screen> {
   @override
   Widget build(BuildContext context) {
     return PackageWidgetLayout(
+      disableSpacer: true,
       buttonText: 'Summary 1/5',
       onButton: () {
         Navigator.of(context).pushNamed(AppRoutes.SUMMARY_2);
       },
       page: -1,
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      child: Expanded(
+        child: ListView(
           children: <Widget>[
             HeaderText.headerTextLight("Summary 1/5"),
-            SizedBox(
-              height: 10.h,
+            AppSizedBox(
+              h: 5.h,
             ),
+            Text(
+              "1. Tell us a bit more",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            AppSizedBox(h: 20.h),
+            Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    border: Border.all(width: 1, color: Colors.grey)),
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      fieldText(
+                        "Where will your Adventure take place?",
+                      ),
+                      valueText(
+                        "100 Queen St W, Toronto",
+                      ),
+                      AppSizedBox(
+                        h: 15.h,
+                      ),
+                      fieldText(
+                        "Which languages will your Adventures be offered in?",
+                      ),
+                      valueText(
+                        "English",
+                      ),
+                      dividerWithMargin(),
+                      fieldText(
+                        "What will your adventure focus on?",
+                      ),
+                      ListTile(
+                        leading: Container(
+                            height: 50.h,
+                            width: 50.w,
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                image:
+                                    AssetImage('assets/images/png/hiking.png'),
+                                fit: BoxFit.contain,
+                              ),
+                            )),
+                        title: valueText("Hiking"),
+                      ),
+                      dividerWithMargin(),
+                      fieldText('Your Experience'),
+                      valueText(
+                          'Yes, I have lead this Adventure professionally'),
+                      dividerWithMargin(),
+                      fieldText('The Perks'),
+                      valueText(
+                          'Travellers could do this on their own, but I bring a unique view to the Adventure.'),
+                      dividerWithMargin(),
+                      fieldText('A Connection'),
+                      valueText(
+                          "I don't like getting too personal with the Travellers.....really??? what??? are you kidding? ok then....."),
+                    ],
+                  ),
+                ))
           ],
         ),
       ),
+    );
+  }
+
+  Text fieldText(String text) {
+    return Text(text, style: TextStyle(fontSize: 14));
+  }
+
+  Text valueText(String text) {
+    return Text(text,
+        style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold));
+  }
+
+  Widget dividerWithMargin() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: Divider(thickness: 1, color: Colors.grey),
     );
   }
 }
