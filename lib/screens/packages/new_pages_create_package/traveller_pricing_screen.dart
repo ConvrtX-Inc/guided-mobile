60e784070aae1ab45935304f7353db77f8fd1093
Emmanuel Dadem <emmadadem@gmail.com>
2022-09-25 16:37:12 +0200

    Complete traveller_pricing_screen.dart

--- lib/screens/packages/new_pages_create_package/traveller_pricing_screen.dart
+++ lib/screens/packages/new_pages_create_package/traveller_pricing_screen.dart
@@ -4,7 +4,9 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:guided/common/widgets/dividers.dart';
 import 'package:guided/common/widgets/package_widgets.dart';
+import 'package:guided/common/widgets/text_flieds.dart';
 import 'package:guided/constants/app_routes.dart';
 import 'package:guided/constants/app_text_style.dart';
 import 'package:guided/utils/package.util.dart';
@@ -24,6 +26,7 @@ class _TravellerPricingScreenState extends State<TravellerPricingScreen> {
   @override
   Widget build(BuildContext context) {
     return PackageWidgetLayout(
+      disableSpacer: true,
       buttonText: 'Next',
       onButton: () {
         if (_formKey.currentState?.validate() != true) {
@@ -34,19 +37,61 @@ class _TravellerPricingScreenState extends State<TravellerPricingScreen> {
             _formKey.currentState!.value);
       },
       page: 17,
-      child: SingleChildScrollView(
-        child: FormBuilder(
-          key: _formKey,
-          onChanged: () {
-            _formKey.currentState!.save();
-          },
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      child: FormBuilder(
+        key: _formKey,
+        onChanged: () {
+          _formKey.currentState!.save();
+        },
+        child: Expanded(
+          child: ListView(
             children: <Widget>[
               HeaderText.headerTextLight("Traveller Pricing"),
               SizedBox(
                 height: 20.h,
               ),
+              Text(
+                "You decide on the price, that's entirely up to you. Enter the price you want the Traveller to pay and discover what you can earn.",
+                style: TextStyle(fontSize: 16),
+              ),
+              AppSizedBox(h: 40.h),
+              AppTextField(
+                name: 'amountPerGuest',
+                hintText: "100",
+                label: 'Individual rate',
+                maxLines: 1,
+                subLabel: 'Each guest pays',
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              Row(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.5,
+                    child: Column(
+                      children: [
+                        Text("Price that will be displayed on the app (15% will go to GuidED)"),
+                        Text(
+                          '115',
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.5,
+                    child: Column(
+                      children: [
+                        Text("Your estimated earnings (5% will go to Guided)"),
+                        Text(
+                          '95',
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
